01da7331ea2367eec28afd750683ee962fd9eca6
Tahlik Betts <supremeod@Tahliks-MacBook-Pro.local>
2025-12-09 12:21:19 -0600

    GarmexAi Foundation V3.9

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'node:url'
 import path from 'node:path'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src'),
-      "@assets": path.resolve(__dirname, '../attached_assets'),
+      "@": path.resolve(process.cwd(), 'src'),
+      "@assets": path.resolve(process.cwd(), '../attached_assets'),
     },
     extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
   },
