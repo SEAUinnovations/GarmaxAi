78316ddb6743291368e47182155f0ad8cff85be6
Tahlik Betts <supremeod@Tahliks-MacBook-Pro.local>
2025-12-08 21:58:57 -0600

    GarmexAi Foundation V3.9

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "../attached_assets"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
+      "@assets": fileURLToPath(new URL('../attached_assets', import.meta.url)),
     },
   },
 })
